65f60aec83c6af9dbab894035c7fec024074e866
Nathat Kuanthanom <nkpeam51@gmail.com>
2021-10-07 21:52:39 +0700

    Number random page v2
    - able to random number

--- lekraidee.js
+++ lekraidee.js
@@ -1,25 +1,29 @@
 
-// function getRandomInt(min, max) {
-//     min = Math.ceil(min);
-//     max = Math.floor(max);
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-// function randomCLick(min,max){
-//     console.log("min:",min)
-//     console.log("max:",max)
-//     let randomNumber = getRandomInt(min,max)
-//     alert(randomNumber)
-//     output.innerHTML = randomNumber
-// }
-function test(min,max){
-    console.log("min:",minValue)
-    console.log("max:",maxValue)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+function randomCLick(){
+    var min = document.getElementById("min-input").value;
+    var max = document.getElementById("max-input").value;
+    
+    let randomNumber = getRandomInt(min,max)
+    console.log("min:",min)
+    console.log("max:",max)
+    console.log("random:",randomNumber)
+    output.innerHTML = randomNumber
+}
+function test(){
+    var min = document.getElementById("min-input").value;
+    var max = document.getElementById("max-input").value;
+    console.log("min:",min)
+    console.log("max:",max)
     output.innerHTML = min
 }
 let output = document.getElementById("output");
-let min = document.getElementById("min-input");
-let max = document.getElementById("max-input");
-let minValue = Number(min.value);
-let maxValue = Number(max.value);
+var min = document.getElementById("min-input").value;
+var max = document.getElementById("max-input").value;
+
 let buttonClick = document.getElementById("randomButton");
-buttonClick.addEventListener("click",test)
+// buttonClick.addEventListener("click",randomCLick(min,max))
